90d1c32bfca017c2ca3df0c969589536051ffb6f
Raghu Betina <raghu@firstdraft.com>
2018-06-07 15:04:41 +0000

    Whitelist columns in Survey for ActiveAdmin

--- app/admin/surveys.rb
+++ app/admin/surveys.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Survey do
+
+ permit_params :survey_name, :user_id, :question_text
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

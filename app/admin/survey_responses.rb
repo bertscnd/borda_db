0fd73025e38cb567ad1d3a9967af6a4ee94d25f6
Raghu Betina <raghu@firstdraft.com>
2018-06-07 15:04:51 +0000

    Whitelist columns in SurveyResponse for ActiveAdmin

--- app/admin/survey_responses.rb
+++ app/admin/survey_responses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register SurveyResponse do
+
+ permit_params :survey_id, :response_id, :response_rank, :submitter_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
